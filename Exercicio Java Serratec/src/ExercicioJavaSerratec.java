e4f6d80cbfdac5ae2575f6506c49a6b6cbfd6494
Adilson Fernando <ornellas00@hotmail.com>
2023-08-27 15:10:45 -0300

    feat: adicionando o exercicio 06 feito

--- Exercicio Java Serratec/src/ExercicioJavaSerratec.java
+++ Exercicio Java Serratec/src/ExercicioJavaSerratec.java
@@ -217,7 +217,24 @@ public class ExercicioJavaSerratec {
 		};
 	};
 	private static void exercicio06() {
-		
+		int lado;
+		do{
+			System.out.print("Digite o lado do quadrado entre um e vinte para fazermos a representação: ");
+			lado = input.nextInt();
+			if(lado <1 || lado >20){
+				System.out.println("Lado do quadrado Invalido, tente novamente ");
+			};
+		}while(lado <=0 || lado >=21);
+		for(int i = 1; i  <= lado ; i++){
+			System.out.println(" ");
+			for( int j =  1 ; j <= lado ; j++){
+				if(i==1 || i==lado || j==1 || j==lado){
+					System.out.print("*");
+				}else{
+					System.out.print(" ");
+				};
+			};
+		};
 	};
 	private static void exercicio07() {
 		
